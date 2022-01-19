a8051911c6dfb05bc67517dab9f9354ac0ca551c
quepas <kiepas.patryk@gmail.com>
2022-01-19 19:48:59 +0100

    Updating library name and the release package preparation script

--- prepare_release_package.sh
+++ prepare_release_package.sh
@@ -4,12 +4,11 @@ mkdir release_package
 # Copy sensor-laser-test binary
 cp bin/sensor-laser-test.exe release_package/
 # Copy RecFusion files (change this path if different!)
-cp "C:/Program Files/ImFusion/RecFusionSDK 1.3.6/bin/." release_package/ -r
-rm release_package/LicenseTool.exe
+cp "C:/Program Files/ImFusion/RecFusion SDK 2.2.0/bin/." release_package/ -r
 rm release_package/ConsoleReconstruction.exe
 rm release_package/MultiViewReconstruction.exe
-rm release_package/MultiViewReconstructionRealSense.exe
 rm release_package/QtReconstruction.exe
 rm release_package/RealSenseReconstruction.exe
+rm release_package/ImFusionResourceCompiler.exe
 "C:\Program Files\7-Zip\7z.exe" a sensor-laser-test_release.zip release_package/.
 rm release_package/ -rf
